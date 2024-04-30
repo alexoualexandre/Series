b651e5afa8a6d48f220a6c26f10dcab37d4fbade
alexandre michiels <alexoualexandre1@gmail.com>
2024-04-30 09:37:40 +0200

    je teste de revenir sur une branche

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const Context = createContext();
 
 export function Retour({ children, Bdd }) {
-  const [menu, setMenu] = useState(true);
+  const [menu, setMenu] = useState(false);
   return (
     <Context.Provider value={{ Bdd, menu, setMenu }}>
       {children}
